9f293d822f8d464fc34126e917bb6f11fdc26318
priya <priyaasinghh345@gmail.com>
2025-12-14 17:48:00 +0530

    add submit logic

--- src/submit.js
+++ src/submit.js
@@ -1,10 +1,43 @@
 // submit.js
+import { useStore } from "./store";
+import { shallow } from "zustand/shallow";
 
+const selector = (state) => ({
+  nodes: state.nodes,
+  edges: state.edges,
+});
 export const SubmitButton = () => {
+  const { nodes, edges } = useStore(selector, shallow);
 
-    return (
-        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-            <button type="submit">Submit</button>
-        </div>
-    );
-}
+  const handleSubmit = async () => {
+    const baseUrl = "http://127.0.0.1:8000";
+    try {
+      const res = await fetch(`${baseUrl}/pipelines/parse`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ nodes, edges }),
+      });
+
+      const data = await res.json();
+      console.log(data);
+    } catch (err) {
+      console.log("error occured", err);
+    }
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <button type="submit" onClick={handleSubmit}>
+        Submit
+      </button>
+    </div>
+  );
+};
